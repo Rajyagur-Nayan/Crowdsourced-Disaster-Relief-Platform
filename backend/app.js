a6e3d4fe2215fecba0b0319b959707028e465afc
Dhruv Boghani <dhruvboghani624@gmail.com>
2025-08-02 21:38:34 +0530

    folder structure and

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors());
-// app.use(bodyParser.json());
+app.use(cors({
+  origin: 'http://localhost:3000', // your frontend URL
+  credentials: true,               // allow cookies
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
